03836ac61ccda102fc44806849c174304193c133
shsunil11 <sunil.shevale@gmail.com>
2015-11-05 08:43:51 -0800

    Added a line

--- com/jp/kafka/KafkaProducer.java
+++ com/jp/kafka/KafkaProducer.java
@@ -3,6 +3,7 @@ public class KafkaProducer {
 
 	public static void main(String[] args) {
 		System.out.println("Kafka Producer has arrived. Testing Git Inegration") ; 
+		System.out.println("Kafka Producer has worked!!! Testing Git Inegration") ; 
 
 	}
 
